45c7f28ba187c01062860525f6302d8698748426
Dinis Costa <61733414+scorpaust@users.noreply.github.com>
2021-11-23 22:46:56 +0000

    Player Movement
    Added movement, facing directions and collisions.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed = 1f;
+
     private Rigidbody2D rb;
+
+    private Animator anim;
+
+    private bool walking;
 
 	private void Awake()
 	{
         rb = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
 	}
 
 	// Start is called before the first frame update
@@ -25,10 +34,34 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
 	{
+        walking = rb.velocity != Vector2.zero;
+
         float horizontalMovement = Input.GetAxisRaw("Horizontal");
 
         float verticalMovement = Input.GetAxisRaw("Vertical");
 
-        rb.velocity = new Vector2(horizontalMovement, verticalMovement);
+        rb.velocity = new Vector2(horizontalMovement, verticalMovement) * moveSpeed;
+
+        anim.SetBool("walk", walking);
+        
+        HandleFacingDirection(horizontalMovement);
+    }
+
+    private void HandleFacingDirection(float horizontalMovement)
+	{
+        Vector3 temp = transform.localScale;
+
+        if (horizontalMovement == 1)
+		{
+            transform.localScale = new Vector3(-0.2f, 0.2f, 1f);
+		}
+        else if (horizontalMovement == -1)
+		{
+            transform.localScale = new Vector3(0.2f, 0.2f, 1f);
+        }
+        else
+		{
+            transform.localScale = temp;
+		}
 	}
 }
